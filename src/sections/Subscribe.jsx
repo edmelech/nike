f1d371c7fe515846b4fe65768d7667acb258690a
Ed Melech <mr.melech@gmail.com>
2023-10-13 17:45:10 +0100

    begin sign up form

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,6 +1,10 @@
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section className="max-container flex justify-between items-center max-lg:flex-col gap-10" id="contact-us">
+      <h3 className="text-4xl leadin-[68px] lg:max-w-md font-palanquin font-bold">Sign Up For 
+      <span className="text-coral-red"> Updates </span> & Newsletters
+      </h3>
+    </section>
   )
 }
 
