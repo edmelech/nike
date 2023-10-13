b0bc7ff79caa692640cea0f49c9b9c310ef232e8
Ed Melech <mr.melech@gmail.com>
2023-10-13 13:52:30 +0100

    complete popular product card and section

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -12,7 +12,7 @@ const PopularProductCard = ({imgURL, name, price}) => {
         <p className='font-montserrat text-xl leading-normal text-slate-gray'>(4.5)</p>
       </div>
       <h3 className='mt-2 text-2xl leading-normal font-semibold font-palanquin'>{name}</h3>
-      <p>{price}</p>
+      <p className='mt-2 font-semibold font-montserrat text-coral-red text-2xl leading-normal'>{price}</p>
     </div>
   )
 }
